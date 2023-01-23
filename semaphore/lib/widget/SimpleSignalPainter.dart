8fdd42f612e006770bd30702949d477a11833296
sunferth <srunferth@carolina.rr.com>
2023-01-23 07:52:52 -0500

    Update SimpleSignalPainter.dart
    Continued progress on person drawing

--- semaphore/lib/widget/SimpleSignalPainter.dart
+++ semaphore/lib/widget/SimpleSignalPainter.dart
@@ -15,6 +15,10 @@ class SimpleSignalPainter extends CustomPainter {
     double torsoLength = 80;
     double legGap = 5;
     double legWidth = 20;
+    double armLength = 80;
+    double shoulderGap = 5;
+    double armWidth = 15;
+    double shoulderHeight = 20;
 
     Paint personFill = Paint();
     personFill.style = PaintingStyle.fill;
@@ -23,7 +27,7 @@ class SimpleSignalPainter extends CustomPainter {
     personStroke.style = PaintingStyle.stroke;
     //personStroke.color = Colors.white;
 
-    canvas.drawCircle(Offset(size.width/2, size.height/2 - 100 ), 25, personFill);
+    canvas.drawCircle(Offset(size.width/2, size.height/2 - 130 ), 25, personFill);
 
     Path path = new Path();
     path.moveTo(size.width/2, size.height/2);
@@ -32,9 +36,25 @@ class SimpleSignalPainter extends CustomPainter {
     path.relativeArcToPoint(Offset(legWidth, 0), radius: Radius.circular(legWidth/2), clockwise: false);
     path.relativeLineTo(0, -legLength);
     path.relativeLineTo(0, -torsoLength);
-    path.relativeLineTo(10, 0);
+    path.relativeLineTo(shoulderGap, 0);
+    path.relativeLineTo(0, armLength);
+    path.relativeArcToPoint(Offset(armWidth, 0), radius: Radius.circular(armWidth/2), clockwise: false);
+    path.relativeLineTo(0, -armLength);
+    path.relativeLineTo(0, -shoulderHeight);
+    path.relativeLineTo(-(armWidth * 2 + shoulderGap * 2 + legWidth * 2 + legGap), 0);
+    path.relativeLineTo(0, shoulderHeight);
+    path.relativeLineTo(0, armLength);
+    path.relativeArcToPoint(Offset(armWidth, 0), radius: Radius.circular(armWidth/2), clockwise: false);
+    path.relativeLineTo(0, -armLength);
+    path.relativeLineTo(shoulderGap, 0);
+    path.relativeLineTo(0, torsoLength);
+    path.relativeLineTo(0, legLength);
+    path.relativeArcToPoint(Offset(legWidth, 0), radius: Radius.circular(legWidth/2), clockwise: false);
+    path.relativeLineTo(0, -legLength);
+    path.relativeLineTo(legGap/2, 0);
+    //TODO round shoulders
 
-    canvas.drawPath(path, personStroke);
+    canvas.drawPath(path, personFill);
   }
 
   @override
